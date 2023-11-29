1a0433ef8a3479038cc0d0fee5fc3a0d95a2d5fd
prudentprogrammer27 <declercq008@gmail.com>
2023-11-29 16:04:08 -0700

    some styling progress

--- js.js
+++ js.js
@@ -1,7 +1,6 @@
 function refreshWeather() {
-    $.get('https://api.open-meteo.com/v1/forecast?latitude=39.4817&longitude=-106.0383&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max,wind_speed_10m_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FDenver&forecast_days=1', (data) => {
-        let highTemp = document.querySelector('h2');
-        highTemp.innerText = (data.daily.temperature_2m_max);
+    $.get('https://api.open-meteo.com/v1/forecast?latitude=39.4817&longitude=-106.0383&daily=temperature_2m_max,apparent_temperature_max,sunrise,sunset,precipitation_probability_max,wind_speed_10m_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FDenver&forecast_days=1', (data) => {
+        console.log(data)
     });
 };
 let refreshBtn = document.getElementById('refresh')
