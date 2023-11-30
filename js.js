fe4b12164f2e026761e269cff709005908f6e256
prudentprogrammer27 <declercq008@gmail.com>
2023-11-29 17:20:40 -0700

    solar times reduced to 24hr format

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 function refreshWeather() {
     $.get('https://api.open-meteo.com/v1/forecast?latitude=39.4817&longitude=-106.0383&daily=temperature_2m_max,temperature_2m_min,apparent_temperature_max,sunrise,sunset,precipitation_probability_max,wind_speed_10m_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FDenver&forecast_days=1', (data) => {
         console.log(data)
+        console.log(data.daily.sunrise[0].slice(11, 16))
         let high = document.querySelector('.weather .temp-high');
         high.innerText = "Today's high: " + (data.daily.temperature_2m_max) + "°";
         let low = document.querySelector('.weather .temp-low');
@@ -8,9 +9,9 @@ function refreshWeather() {
         let precip = document.querySelector('.weather .precipitation');
         precip.innerText = "Chance of precipitation: " +(data.daily.precipitation_probability_max)+ '%';
         let sunrise = document.querySelector('.weather .sunrise');
-        sunrise.innerText = "Sunrise: " + (data.daily.sunrise);
+        sunrise.innerText = "Sunrise: " + (data.daily.sunrise[0].slice(11, 16));
         let sunset = document.querySelector('.weather .sunset');
-        sunset.innerText = "Sunset: " + (data.daily.sunset);
+        sunset.innerText = "Sunset: " + (data.daily.sunset[0].slice(11, 16));
     });
 };
 let refreshBtn = document.getElementById('refresh')
