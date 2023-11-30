937500851479df8783dfb5df53feba526ff1769f
prudentprogrammer27 <declercq008@gmail.com>
2023-11-30 14:51:00 -0700

    trying to add audio - embedded content no autoplay on Chrome

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
-function refreshWeather() {
-    $.get('https://api.open-meteo.com/v1/forecast?latitude=39.4817&longitude=-106.0383&daily=temperature_2m_max,temperature_2m_min,apparent_temperature_max,sunrise,sunset,precipitation_probability_max,wind_speed_10m_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FDenver&forecast_days=1', (data) => {
+function refreshWeather(url) {
+    $.get(url, (data) => {
         console.log(data)
         console.log(data.daily.sunrise[0].slice(11, 16))
         let high = document.querySelector('.weather .temp-high');
@@ -15,8 +15,18 @@ function refreshWeather() {
     });
 };
 let refreshBtn = document.getElementById('refresh')
-refreshBtn.addEventListener('click', refreshWeather)
-
-
-
-
+refreshBtn.addEventListener('click', function() {
+    refreshWeather('https://api.open-meteo.com/v1/forecast?latitude=39.4817&longitude=-106.0383&daily=temperature_2m_max,temperature_2m_min,apparent_temperature_max,sunrise,sunset,precipitation_probability_max,wind_speed_10m_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FDenver&forecast_days=1')
+    let city = document.querySelector('.weather .city')
+    city.innerText = 'Breckenridge';
+    let breckLogo = document.querySelector('img');
+    breckLogo.src = 'Breck.jpg'
+});
+let Detroit = document.getElementById('Detroit');
+Detroit.addEventListener('click', function() {
+    refreshWeather('https://api.open-meteo.com/v1/forecast?latitude=42.3314&longitude=-83.0457&daily=temperature_2m_max,temperature_2m_min,apparent_temperature_max,sunrise,sunset,precipitation_probability_max,wind_speed_10m_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FDenver&forecast_days=1');
+    let city = (document.querySelector('.weather .city'));
+    city.innerText = "Detroit"
+    let DetroitLogo = document.querySelector('img');
+    DetroitLogo.src = 'Detroit.png'
+});
